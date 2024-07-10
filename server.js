7e30abb00a38e9fd0f37acedc1731b091dd37877
mdshakebakhtar003 <160420527+mdshakebakhtar003@users.noreply.github.com>
2024-07-10 21:04:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express')
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const flash = require("connect-flash");
+const session = require('express-session'); // Import the session middleware
 const db = require('./db');
 require('dotenv').config();
-
-const bodyParser = require('body-parser'); 
-app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.send('hello'); // Respond with 'hello' when a GET request is made to '/'
-});
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Configure the session middleware
+app.use(session({
+  secret: 'guud', // Replace with a strong secret key
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
+}));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+// Initialize the flash middleware
+app.use(flash());
+
+
+
+
 // Import the router files
 const userRoutes = require('./routes/userRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
-
+const indexRoutes = require('./routes/home');
 // Use the routers
+app.use('/', indexRoutes);
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
